25485095ef3ca40460ae0326d33e34055f047e6a
Ted McCormick <66527737+edwardmccormick@users.noreply.github.com>
2020-11-12 14:53:33 -0600

    broke the branching

--- code.js
+++ code.js
@@ -2,3 +2,26 @@
 function helloWorld() {
     return "Hello, World!";
 }
+function sayHello(x) {
+   if (typeof x !=="string") {return "Hello, World!"}
+   else if (!isNaN(x)) {return "Hello, World!"}
+   else {return "Hello, " + x +"!"};
+// return "Hello, Jane!"
+}
+
+function isFive(x) {
+return parseFloat(x)===5
+}
+
+function isEven(x) {
+   if(x===false) {return false} else {return x % 2 ==0}
+}
+
+function isVowel(x) {
+    if (typeof x !== "string") {return false}
+    else {return !/[bcdfghjklmnpqrstvwxyz]/.test(x)};
+}
+
+function add(x,y) {
+    if (isNaN(x,y)) {return parseFloat(("banana"))} else {return parseFloat(x) + parseFloat(y)};
+}
